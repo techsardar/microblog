3525ba01cb5c89e654543bfd29ff52183e6ed6a4
Sardar Syed <sardar@gmail.com>
2021-09-01 19:54:01 +0530

    Post model added. SQL Alchemy & migrations added.

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or ')JKL:NLRV23L.&*m<*opjkl&(*L:IopU'
+    SECRET_KEY = os.environ.get('SECRET_KEY', ')JKL:NLRV23L.&*m<*opjkl&(*L:IopU')
     
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', f"sqlite:///{os.path.join(basedir, 'app.db')}")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
